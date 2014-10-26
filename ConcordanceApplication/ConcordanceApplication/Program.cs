ae212265eec81512076abd0643e36b7076f7c991
VladCherniavsky <vlad.cherniavsky@gmail.com>
2014-10-26 23:37:24 +0300

    task_2

---
+++ ConcordanceApplication/ConcordanceApplication/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConcordanceApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string lines = File.ReadAllText(path: @"C:\Users\Влад\Documents\Task-1\ConcordanceApplication\Text.txt");
+            Console.WriteLine(lines);
+
+            string[] words = SplitWords(lines);
+
+            int i = 0;
+            foreach (var  word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        static string[] SplitWords(string s)
+            {
+                return Regex.Split(s, @"\W+");
+            }
+
+
+           /* foreach (string line in
+                    File.ReadAllLines(path: @"C:\Users\Влад\Documents\Task-1\ConcordanceApplication\Text.txt"))
+            {
+                string[] parts = line.SplitWords(line);//Split(new char[] {' ', ',', '.',});
+                foreach (string part in parts)
+                {
+                    Console.WriteLine("{0}", part);
+                }
+
+            }
+        }
+
+            */
+        
+
+/*
+                int i = 0;
+
+            foreach (string line in File.ReadAllLines(path: @"C:\Users\Влад\Documents\Task-1\ConcordanceApplication\Text.txt"))
+            {
+                string[] parts = line.Split(' ');
+                foreach (string part in parts)
+                {
+                    Console.WriteLine("{0}:{1}", i, part);
+                }
+                i++;
+            }*/
+
+
+
+        }
+
+        
+    }
+
---
+++ ConcordanceApplication/ConcordanceApplication/Text.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConcordanceApplication
+{
+    public class Text
+    {
+        public static string[] SplitWords(string s)
+        {
+            return Regex.Split(s, @"\W+");
+        }
+    }
+}
